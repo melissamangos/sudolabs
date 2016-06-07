7b45ea78e38e46dc7babe86c0d0b29f0d5408046
melissamangos <mmangos32@gmail.com>
2016-06-06 23:30:54 -0400

    Added things that don't work to JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,14 @@ var mentor_blurb;
 var instructor_blurb;
 var team_blurb;
 
+var work_left;
+var work_middle;
+var work_right;
+
+var hidden_left;
+var hidden_middle;
+var hidden_right;
+
 var mentor = 0;
 var instruc = 0;
 var team = 0;
@@ -17,6 +25,20 @@ window.onload = function() {
 	mentor_blurb.parentNode.removeChild(mentor_blurb);
 	instructor_blurb.parentNode.removeChild(instructor_blurb);
 	team_blurb.parentNode.removeChild(team_blurb);
+
+	hidden_left = document.getElementById("hidden-left");
+	hidden_middle = document.getElementById("hidden-middle");
+	hidden_right = document.getElementById("hidden-right");
+
+	hidden_left.parentNode.removeChild(hidden_left);
+	hidden_middle.parentNode.removeChild(hidden_middle);
+	hidden_right.parentNode.removeChild(hidden_right);
+
+	work_left = document.getElementById("workshop-left");
+	work_middle = document.getElementById("workshop-middle");
+	work_right = document.getElementById("workshop-right");
+
+
 }
 
 function volunteerDrop(parent) {
@@ -94,19 +116,44 @@ function closeAnimation(elem) {
 }
 
 function extendWorkshop(elem) {
+	switch (elem.id) {
+		case "workshop-left":
+			// console.log(hidden_left);
+			elem.appendChild(hidden_left);
+			// openWorkshop(hidden_left);
+			// document.getElementById("workshop-list").removeChild(work_middle);
+			// document.getElementById("workshop-list").removeChild(work_right);
+			break;
+		case "workshop-right":
+			elem.appendChild(hidden_right);
+			document.getElementById("workshop-list").removeChild(work_middle);
+			document.getElementById("workshop-list").removeChild(work_left);
+			break;
+		case "workshop-middle":
+			elem.appendChild(hidden_middle);
+			document.getElementById("workshop-list").removeChild(work_left);
+			document.getElementById("workshop-list").removeChild(work_right);
+			break;
+	}
+
+	// elem.appendChild(child);
 	openWorkshop(elem);
 
 }
 
 function openWorkshop(elem) {
+	var text_stay = elem.childNodes[1];
 	var width = 30;
-	var id = setInterval(frame, 10);
+	var id = setInterval(frame, 5);
+	text_stay.style.width = "30vw";
+	// text_stay.style.marginLeft = "0px";
 	function frame() {
-		if (width == 70) {
+		if (width == 80) {
 			clearInterval(id);
 		} else {
 			width++;
 			elem.style.width = width + "vw";
+			text_stay.style.width = "30vw";
 		}
 	}
 }
