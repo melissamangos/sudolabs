93bc655b104aaa97059913e3bc9d274fa50886f9
melissamangos <mmangos32@gmail.com>
2016-06-08 01:25:49 -0400

    Added smooth scrolling & relevant links

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,18 @@ var mentor_blurb;
 var instructor_blurb;
 var team_blurb;
 
-var work_left;
-var work_middle;
-var work_right;
-
-var hidden_left;
-var hidden_middle;
-var hidden_right;
-
 var mentor = 0;
 var instruc = 0;
 var team = 0;
 
+
+/**************************************
+FUNCTION: 	window.onload
+
+PURPOSE: 	Removes paragraphs in 
+			Volunteer section to be
+			added once clicked
+**************************************/
 window.onload = function() {
 	mentor_blurb = document.getElementById("mentors");
 	instructor_blurb = document.getElementById("instructors");
@@ -25,62 +25,85 @@ window.onload = function() {
 	mentor_blurb.parentNode.removeChild(mentor_blurb);
 	instructor_blurb.parentNode.removeChild(instructor_blurb);
 	team_blurb.parentNode.removeChild(team_blurb);
-
-	hidden_left = document.getElementById("hidden-left");
-	hidden_middle = document.getElementById("hidden-middle");
-	hidden_right = document.getElementById("hidden-right");
-
-	hidden_left.parentNode.removeChild(hidden_left);
-	hidden_middle.parentNode.removeChild(hidden_middle);
-	hidden_right.parentNode.removeChild(hidden_right);
-
-	work_left = document.getElementById("workshop-left");
-	work_middle = document.getElementById("workshop-middle");
-	work_right = document.getElementById("workshop-right");
-
-
 }
 
-function volunteerDrop(parent) {
-	makeActive(parent);
-	switch(parent.id) {
+/**************************************
+FUNCTION: 	volunteerDrop
+
+ARGUEMENTS: elem - sends the current 
+			element clicked to append
+			or remove the respective 
+			volunteer paragraph
+
+PURPOSE: 	Append or remove the blurb
+			under each volunteer tab
+**************************************/
+function volunteerDrop(elem) {
+	makeActive(elem);
+	switch(elem.id) {
 		case "mentor-full":
 			if (mentor == 0) {
-				openAnimation(parent);
-				parent.appendChild(mentor_blurb);
+				openAnimation(elem);
+				elem.appendChild(mentor_blurb);
 
 				mentor = 1;
 			} else {
-				closeAnimation(parent);
-				parent.removeChild(mentor_blurb);
+				closeAnimation(elem);
+				elem.removeChild(mentor_blurb);
 				mentor = 0;
 			}
 			break;
 		case "instructor-full":
 			if (instruc == 0) {
-				openAnimation(parent);
-				parent.appendChild(instructor_blurb);
+				openAnimation(elem);
+				elem.appendChild(instructor_blurb);
 				instruc = 1;
 			} else {
-				closeAnimation(parent);
-				parent.removeChild(instructor_blurb);
+				closeAnimation(elem);
+				elem.removeChild(instructor_blurb);
 				instruc = 0;
 			}
 			break;
 		case "team-full":
 			if (team == 0) {
-				openAnimation(parent);
-				parent.appendChild(team_blurb);
+				openAnimation(elem);
+				elem.appendChild(team_blurb);
 				team = 1;
 			} else {
-				closeAnimation(parent);
-				parent.removeChild(team_blurb);
+				closeAnimation(elem);
+				elem.removeChild(team_blurb);
 				team = 0;
 			}
 			break;
 	}
 }
 
+/**************************************
+FUNCTION: 	forceOpen
+
+ARGUEMENTS: id - id of the volunteer
+			element that is forced 
+			to open
+
+PURPOSE: 	Force a volunteer element
+			open when redirected to
+			volunteer
+**************************************/
+function forceOpen(id) {
+	var elem = document.getElementById(id);
+	volunteerDrop(elem);
+}
+
+/**************************************
+FUNCTION: 	makeActive
+
+ARGUEMENTS: elem - sends current element 
+
+PURPOSE: 	Add or remove the 'active' 
+			class from an element to
+			check if a volunteer tab 
+			is open or closed
+**************************************/
 function makeActive(elem) {
 	if (elem.classList.contains("active")) {
 		elem.classList.remove("active");
@@ -89,77 +112,140 @@ function makeActive(elem) {
 	}
 }
 
+/**************************************
+FUNCTION: 	openAnimation
+
+ARGUEMENTS: elem - sends current element 
+
+PURPOSE: 	Animates a volunteer element
+			expanding. Uses height in 
+			pixels & setInterval determines
+			to length of animation
+**************************************/
 function openAnimation(elem) {
-    var height = 42;
-    var id = setInterval(frame, 1);
-    function frame() {
-        if (height == 177) {
-            clearInterval(id);
-        } else {
-            height = height + 5; 
-            elem.style.height = height + 'px'; 
-        }
-    }
+	var height = 42;
+	var id = setInterval(frame, 1);
+	function frame() {
+		if (height == 177) {
+			clearInterval(id);
+		} else {
+			height = height + 5; 
+			elem.style.height = height + 'px'; 
+		}
+	}
 }
 
+/**************************************
+FUNCTION: 	closeAnimation
+
+ARGUEMENTS: elem - sends current element 
+
+PURPOSE: 	Animates a volunteer element
+			closing. Uses height in 
+			pixels & setInterval determines
+			to length of animation
+**************************************/
 function closeAnimation(elem) {
-    var height = 177;
-    var id = setInterval(frame, 1);
-    function frame() {
-        if (height <= 42) {
-            clearInterval(id);
-        } else {
-            height = height - 5; 
-            elem.style.height = height + 'px'; 
-        }
-    }
+	var height = 178;
+	var id = setInterval(frame, 1);
+	function frame() {
+		if (height <= 42) {
+			clearInterval(id);
+		} else {
+			height = height - 2; 
+			elem.style.height = height + 'px'; 
+		}
+	}
 }
 
+/**************************************
+FUNCTION: 	extendWorkshop
+
+ARGUEMENTS: elem - sends current element 
+
+PURPOSE: 	Adds or removes class 'expand'
+			to a workshop element
+**************************************/
 function extendWorkshop(elem) {
-	// elem.parentNode.toggleClass("expand");
 	if (elem.classList.contains("expand")) {
 		elem.classList.remove("expand");
 	} else {
 		elem.classList.add("expand");
 	}
-	// switch (elem.id) {
-	// 	case "workshop-left":
-	// 		// console.log(hidden_left);
-	// 		elem.appendChild(hidden_left);
-	// 		// openWorkshop(hidden_left);
-	// 		// document.getElementById("workshop-list").removeChild(work_middle);
-	// 		// document.getElementById("workshop-list").removeChild(work_right);
-	// 		break;
-	// 	case "workshop-right":
-	// 		elem.appendChild(hidden_right);
-	// 		document.getElementById("workshop-list").removeChild(work_middle);
-	// 		document.getElementById("workshop-list").removeChild(work_left);
-	// 		break;
-	// 	case "workshop-middle":
-	// 		elem.appendChild(hidden_middle);
-	// 		document.getElementById("workshop-list").removeChild(work_left);
-	// 		document.getElementById("workshop-list").removeChild(work_right);
-	// 		break;
-	// }
-
-	// // elem.appendChild(child);
-	// openWorkshop(elem);
-
 }
 
-// function openWorkshop(elem) {
-// 	var text_stay = elem.childNodes[1];
-// 	var width = 30;
-// 	var id = setInterval(frame, 5);
-// 	text_stay.style.width = "30vw";
-// 	// text_stay.style.marginLeft = "0px";
-// 	function frame() {
-// 		if (width == 80) {
-// 			clearInterval(id);
-// 		} else {
-// 			width++;
-// 			elem.style.width = width + "vw";
-// 			text_stay.style.width = "30vw";
-// 		}
-// 	}
-// }
+/**************************************
+NOTE: THE FOLLOWING FUNCTIONS ARE NOT OURS
+**************************************/
+
+/**************************************
+FUNCTION: 	currentYPosition
+
+PURPOSE: 	Determines Y position on 
+			the page for scrolling
+**************************************/
+function currentYPosition() {
+	// Firefox, Chrome, Opera, Safari
+	if (self.pageYOffset) return self.pageYOffset;
+	// Internet Explorer 6 - standards mode
+	if (document.documentElement && document.documentElement.scrollTop)
+		return document.documentElement.scrollTop;
+	// Internet Explorer 6, 7 and 8
+	if (document.body.scrollTop) return document.body.scrollTop;
+	return 0;
+}
+
+/**************************************
+FUNCTION: 	elmYPosition
+
+ARGUEMENTS: eID - ID of element to 
+			scroll to
+
+PURPOSE: 	Determines Y coordinate of
+			destination element
+**************************************/
+function elmYPosition(eID) {
+	var elem = document.getElementById(eID);
+	var y = elem.offsetTop;
+	var node = elem;
+	while (node.offsetParent && node.offsetParent != document.body) {
+		node = node.offsetParent;
+		y += node.offsetTop;
+	} return y;
+}
+
+/**************************************
+FUNCTION: 	smoothScroll
+
+ARGUEMENTS: eID - ID of element to 
+			scroll to
+
+PURPOSE: 	Takes the current position,
+			and the position of the 
+			destination, determines 
+			speed to scroll and animates
+			page scrolling
+**************************************/
+function smoothScroll(eID) {
+	var startY = currentYPosition();
+	var stopY = elmYPosition(eID) - 60;
+	var distance = stopY > startY ? stopY - startY : startY - stopY;
+	if (distance < 100) {
+		scrollTo(0, stopY); return;
+	}
+	var speed = Math.round(distance / 100);
+	if (speed >= 20) speed = 20;
+	var step = Math.round(distance / 25);
+	var leapY = stopY > startY ? startY + step : startY - step;
+	var timer = 0;
+	if (stopY > startY) {
+		for ( var i=startY; i<stopY; i+=step ) {
+			setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+			leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+		} return;
+	}
+	for ( var i=startY; i>stopY; i-=step ) {
+		setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+		leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+	}
+}
